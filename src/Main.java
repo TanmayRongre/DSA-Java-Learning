45ba97ba4d8a19db1ef6597395a11dd99a0df3a7
Tanmay Rongre <tanmayrongre2008@gmail.com>
2026-01-18 22:05:04 +0530

    Day 0: Hello World and Loop logic

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // This is the standard way to print in Java
+        System.out.println("Hello World! This is my first Java push.");
+
+        // Let's add a loop to test logic
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Count: " + i);
+        }
+    }
+}
